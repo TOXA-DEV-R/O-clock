6822c8c72e52d69fbbf9e92e07ebefc77316f0a2
Tohir <toxa.tb9@gmail.com>
2021-05-23 07:29:47 +0600

    start oclock_02

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 "use string";
+// watch-01 start
 const oclockSeconds = document.querySelector(".oclock-seconds-init");
 const oclockMinutes = document.querySelector(".oclock-minutes-init");
 const oclockHours = document.querySelector(".oclock-hours-init");
@@ -39,3 +40,19 @@ setFun();
 setInterval(() => {
   setFun();
 }, 1000);
+// watch-01 end
+
+// watch-02 start
+const watch = document.querySelector(".watch span");
+
+function formatAMPM(date) {
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var ampm = hours >= 12 ? "pm" : "am";
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  var strTime = hours + ":" + minutes + " " + ampm;
+  return strTime;
+}
+// watch-02 end
