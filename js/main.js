e8beabf89288027bdb01a6da7d2d9b219b47fece
Tohir <toxa.tb9@gmail.com>
2021-05-23 09:46:37 +0600

    finish oclock_02

--- js/main.js
+++ js/main.js
@@ -1,58 +1,80 @@
 "use string";
 // watch-01 start
-const oclockSeconds = document.querySelector(".oclock-seconds-init");
-const oclockMinutes = document.querySelector(".oclock-minutes-init");
-const oclockHours = document.querySelector(".oclock-hours-init");
+(function circleOclock() {
+  const oclockSeconds = document.querySelector(".oclock-seconds-init");
+  const oclockMinutes = document.querySelector(".oclock-minutes-init");
+  const oclockHours = document.querySelector(".oclock-hours-init");
 
-let dateGl = new Date();
-let seconds = dateGl.getSeconds();
-let minutes = Math.floor(dateGl.getMinutes() * 6);
-let hours = Math.floor(dateGl.getHours() % 12);
+  let dateGl = new Date();
 
-let hoursIndicator = 30 * hours;
+  let seconds = dateGl.getSeconds();
+  let minutes = Math.floor(dateGl.getMinutes() * 6);
+  let hours = Math.floor(dateGl.getHours() % 12);
 
-hoursIndicator = Math.floor(hoursIndicator + minutes / 12);
+  let hoursIndicator = 30 * hours;
 
-let countHours = 0;
+  hoursIndicator = Math.floor(hoursIndicator + minutes / 12);
 
-function setFun() {
-  oclockSeconds.style.transform = `rotate(${seconds}deg)`;
-  oclockMinutes.style.transform = `rotate(${minutes}deg)`;
-  oclockHours.style.transform = `rotate(${hoursIndicator}deg)`;
-  seconds += 6;
+  let countHours = 0;
 
-  if (seconds >= 358 && seconds <= 362) {
-    seconds = 0;
-    minutes += 6;
-    countHours++;
+  function setFun() {
+    oclockSeconds.style.transform = `rotate(${seconds}deg)`;
+    oclockMinutes.style.transform = `rotate(${minutes}deg)`;
+    oclockHours.style.transform = `rotate(${hoursIndicator}deg)`;
+    seconds += 6;
 
-    if (countHours === 12) {
-      hoursIndicator += 6;
-      countHours = 0;
+    if (seconds >= 358 && seconds <= 362) {
+      seconds = 0;
+      minutes += 6;
+      countHours++;
+
+      if (countHours === 12) {
+        hoursIndicator += 6;
+        countHours = 0;
+      }
+    }
+
+    if (minutes >= 358 && minutes <= 362) {
+      minutes = 0;
     }
   }
-
-  if (minutes >= 358 && minutes <= 362) {
-    minutes = 0;
-  }
-}
-setFun();
-setInterval(() => {
   setFun();
-}, 1000);
+  setInterval(() => {
+    setFun();
+  }, 1000);
+})();
 // watch-01 end
 
 // watch-02 start
-const watch = document.querySelector(".watch span");
+(function arrayOclock() {
+  const watchSpan = document.querySelector(".watch span");
+  const watchDiv = document.querySelector(".watch div");
 
-function formatAMPM(date) {
-  var hours = date.getHours();
-  var minutes = date.getMinutes();
-  var ampm = hours >= 12 ? "pm" : "am";
-  hours = hours % 12;
-  hours = hours ? hours : 12; // the hour '0' should be '12'
-  minutes = minutes < 10 ? "0" + minutes : minutes;
-  var strTime = hours + ":" + minutes + " " + ampm;
-  return strTime;
-}
+  function formatAMPM() {
+    let dateGl = new Date();
+    let hours = dateGl.getHours();
+    let minutes = dateGl.getMinutes();
+    let seconds = dateGl.getSeconds();
+
+    let ampm = hours >= 12 ? "pm" : "am";
+    hours %= 12;
+    hours = hours ? hours : 12;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+    return hours + ":" + minutes + ":" + seconds + " " + ampm;
+  }
+
+  function fullTimes() {
+    let dateGl = new Date();
+    return (
+      dateGl.getDate() + " " + dateGl.getMonth() + " " + dateGl.getUTCFullYear()
+    );
+  }
+  fullTimes();
+  formatAMPM();
+  setInterval(() => {
+    watchDiv.innerHTML = fullTimes();
+    watchSpan.innerHTML = formatAMPM();
+  }, 1000);
+})();
 // watch-02 end
